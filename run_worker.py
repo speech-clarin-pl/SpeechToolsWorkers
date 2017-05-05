7cb6f936398744209a87d0ca69525b821bbee5ae
Danijel Koržinek <danijel@pja.edu.pl>
2017-05-05 12:08:22 +0200

    Added user/group switching.

--- run_worker.py
+++ run_worker.py
@@ -10,9 +10,11 @@ import workers
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run worker program')
     parser.add_argument('worker', help='Name of worker to run. Examples: ffmpeg, normalization, speech')
+    parser.add_argument('--log', '-l', help='log to file instead of screen')
     parser.add_argument('--daemon', '-d', help='run as daemon', action='store_true')
-    parser.add_argument('--log', '-l', help='log to file')
-    parser.add_argument('--pidfile', '-p', help='setup a pid lockfile')
+    parser.add_argument('--pidfile', '-p', help='setup a pid lockfile of daemon (required!)')
+    parser.add_argument('--user', '-u', help='set user of daemon')
+    parser.add_argument('--group', '-g', help='set group of daemon')
 
     args = parser.parse_args()
 
@@ -40,7 +42,8 @@ if __name__ == '__main__':
             print 'PID file is required for daemon mode!'
             exit(1)
 
-        d = daemonize.Daemonize(app=os.path.basename(sys.argv[0]), pid=args.pidfile, action=action, keep_fds=keep_fds)
+        d = daemonize.Daemonize(app=os.path.basename(sys.argv[0]), pid=args.pidfile, action=action, keep_fds=keep_fds,
+                                user=args.user, group=args.group)
         d.start()
     else:
         action()
