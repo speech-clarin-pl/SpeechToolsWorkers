d1c52ffcbdff4508a46272681639fbdcd0bcaca8
Danijel Koržinek <danijel@pja.edu.pl>
2017-12-08 19:50:08 +0100

    Better logging.

--- tasks/audio.py
+++ tasks/audio.py
@@ -2,12 +2,15 @@ import os
 from subprocess import call, STDOUT
 from tempfile import mkstemp
 
+from config import logger
+
 
 def ffmpeg(dir, file):
     fd, tmp = mkstemp(dir=dir, suffix='.wav')
     os.close(fd)
 
     cmd = ['ffmpeg', '-y', '-i', os.path.join(dir, file), '-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16k', tmp]
+    logger.info(u'Running {}'.format(' '.join(cmd)))
     try:
         with open(tmp + '_ffmpeg.log', 'w') as f:
             call(cmd, stdout=f, stderr=STDOUT)
--- tasks/text.py
+++ tasks/text.py
@@ -3,6 +3,8 @@ import os
 import re
 from tempfile import mkstemp
 
+from config import logger
+
 pat = re.compile('[^\w\s]', flags=re.U)
 num = re.compile('[0-9]', flags=re.U)
 ws = re.compile('\s+', flags=re.U)
@@ -11,6 +13,8 @@ ws = re.compile('\s+', flags=re.U)
 def normalize(dir, file):
     fd, output = mkstemp(dir=dir)
     os.close(fd)
+    outputname = os.path.basename(output)
+    logger.info(u'Normalizing text file {} -> {}'.format(file, outputname))
     with codecs.open(os.path.join(dir, file), encoding='utf8') as fin:
         with codecs.open(output, 'w', encoding='utf8') as fout:
             for line in fin:
@@ -19,4 +23,4 @@ def normalize(dir, file):
                 line = num.sub(' ', line)
                 line = ws.sub(' ', line)
                 fout.write(line)
-    return os.path.basename(output)
+    return outputname
